aca6190c0725f66fc11c225fa92bd270a0b180e1
Kibreab Aklilu <Akkeba37@gmail.com>
2024-04-03 21:39:39 +0300

    Add database connection setup

---
+++ backend/database/connect.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	var err error
+	dsn := os.Getenv("DATABASE_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to databse")
+	}
+	fmt.Println("Successfully connected to PostgreSQL")
+	// db.AutoMigrate(&domain.Product{}, &domain.User{}, &domain.Blog{}, &domain.Comment{})
+}
--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"backend/database"
 
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	database.ConnectDB()
+}
+
+func main() {
+	router := gin.Default()
+	router.Run()
 }
